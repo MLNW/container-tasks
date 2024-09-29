3c9a317bf951516ba3dc1e759ffce0cc0401d3ae
Lucas Resch <lucas.resch@gmx.de>
2024-09-29 14:34:50 +0200

    Install dive

---
+++ images/dev/resources/tmp/scripts/install-dive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e "Please run as root"
+  exit 1
+fi
+
+name="dive"
+archive="$name.tar.gz"
+repo="wagoodman/dive"
+artifact="linux_amd64.tar.gz"
+
+set -xe
+
+temp_dir=$(mktemp -d)
+trap 'rm -rf "$temp_dir"' EXIT
+
+pushd "$temp_dir" || exit 1
+
+url=$(curl -s https://api.github.com/repos/$repo/releases/latest \
+  | jq -r '.assets[] | .browser_download_url' \
+  | grep -e "$artifact")
+curl -L -o $archive $url
+
+tar xzf $archive
+rm $archive
+
+install -o root -g root -m 0755 $name /usr/local/bin/$name
+
+popd || exit 1

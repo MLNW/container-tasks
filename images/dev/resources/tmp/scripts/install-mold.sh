0c911bc829f782572601d5a693bc49888ee81adb
Lucas Resch <lucas.resch@gmx.de>
2024-10-03 09:08:48 +0200

    Install mold for faster linking

---
+++ images/dev/resources/tmp/scripts/install-mold.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e "Please run as root"
+  exit 1
+fi
+
+name="mold"
+archive="$name.tar.gz"
+repo="rui314/mold"
+artifact="mold-.*-x86_64-linux.tar.gz"
+
+set -xe
+
+temp_dir=$(mktemp -d)
+trap 'rm -rf "$temp_dir"' EXIT
+
+pushd "$temp_dir" || exit 1
+
+url=$(curl -s https://api.github.com/repos/$repo/releases/latest \
+  | jq -r '.assets[] | .browser_download_url' \
+  | grep -e "$artifact")
+curl -L -o $archive $url
+
+tar xzf $archive --strip-components=1
+rm $archive
+
+install -o root -g root -m 0755 bin/$name /usr/local/bin/$name
+
+popd || exit 1

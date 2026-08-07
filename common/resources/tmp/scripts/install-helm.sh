52cb3a204d3524a1c2e3c36f46574c12c3059906
Lucas Resch <lucas.resch@gmx.de>
2026-08-07 17:19:22 +0200

    Update helm to v4.2.3

--- common/resources/tmp/scripts/install-helm.sh
+++ common/resources/tmp/scripts/install-helm.sh
@@ -5,4 +5,27 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+if [[ $# -ne 1 ]]; then
+  echo -e "Please provide the version to install, e.g., $0 v4.2.3"
+  exit 1
+fi
+
+version=$1
+archive="helm-$version-linux-amd64.tar.gz"
+
+set -xe
+
+temp_dir=$(mktemp -d)
+trap 'rm -rf "$temp_dir"' EXIT
+
+pushd "$temp_dir" || exit 1
+
+curl -LO "https://get.helm.sh/$archive"
+curl -LO "https://get.helm.sh/$archive.sha256sum"
+sha256sum --check "$archive.sha256sum"
+
+tar -xzf "$archive" --strip-components=1 linux-amd64/helm
+
+install -o root -g root -m 0755 helm /usr/local/bin/helm
+
+popd || exit 1
